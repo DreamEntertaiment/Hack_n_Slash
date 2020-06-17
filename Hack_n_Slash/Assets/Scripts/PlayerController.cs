cd87436c8c6d9b77d0174ccecaba63f27649db60
moritz <zocken223344@gmx.de>
2020-06-17 14:50:27 +0200

    Added Baisc Player/CameraMovement
    Implemented basic Player and CameraMovement
    Last commit without making a extra branch pls!! :)

---
+++ Hack_n_Slash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        PlayerRotation();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+
+    private void PlayerRotation()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+
+}
