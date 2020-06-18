a2eb4cdc3bdd2e05f54f9fd1a45ebb0169f79493
moritz <zocken223344@gmx.de>
2020-06-18 09:51:44 +0200

    Optemizing PlayerController
    Optemized PlayerController Added All OnClick funktions needed cleanuped some code + comments :P

--- Hack_n_Slash/Assets/Scripts/PlayerController.cs
+++ Hack_n_Slash/Assets/Scripts/PlayerController.cs
@@ -18,26 +18,101 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMovement();
-        PlayerRotation();
+        AttackHandler();
     }
-
+    /// <summary>
+    /// Player Movement and Rotation(Player is looking at the cursor position)
+    /// </summary>
     private void PlayerMovement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 movement = new Vector3(horizontal, 0, vertical);
-        transform.Translate(movement * movementSpeed * Time.deltaTime);
-    }
-
-    private void PlayerRotation()
-    {
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        //Player Rotation
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if(Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+    }
+
+    private void AttackHandler()
+    {
+        LMBclick();
+        RMBclick();
+        Qclick();
+        Eclick();
+        Rclick();
+        SHIFTclick();
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void LMBclick()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //NormalAttack
+            Debug.Log("LMB");
+        }
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void RMBclick()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            //EyeFrame
+            Debug.Log("RMB");
+        }
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void Qclick()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //GarenE
+            Debug.Log("Q");
+        }
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void Eclick()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //GravesUlt
+            Debug.Log("E");
+        }
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void Rclick()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //CanonBall
+            Debug.Log("R");
+        }
+    }
+    /// <summary>
+    /// Erklärung zum code bidde hier ^^
+    /// </summary>
+    private void SHIFTclick()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            //Dash
+            Debug.Log("SWUSCH!!");
         }
     }
 
